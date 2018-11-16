c8dc373b1b2f8335b4fff59cd30602376bb1a656
yongpilMoon <m71661@gmail.com>
2018-11-17 01:18:33 +0900

    Router를 세팅한다

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+class Root extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={App} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 serviceWorker.unregister();
