de090c3216d47d9a1e2ae99d1f3f0479c437c5b3
yongpilMoon <m71661@gmail.com>
2018-11-17 18:02:54 +0900

    테스트 관련 모듈을 설치하고 enzyme 세팅 파일을 추가한다

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from '../components/App';
+import {shallow} from "enzyme/build";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('Register', () => {
+  it('renders correctly', () => {
+    const component = shallow(<App/>);
+    expect(component).toMatchSnapshot();
+  });
+})
